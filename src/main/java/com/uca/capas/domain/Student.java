674f8bae74a620edfa66ca26acb33b7bdf54bf99
cortezle <00029117@uca.edu.sv>
2020-05-02 23:00:39 -0600

    avance

---
+++ src/main/java/com/uca/capas/domain/Student.java
@@ -0,0 +1,42 @@
+package com.uca.capas.domain;
+
+public class Student {
+	private String name;
+	private String apellido;
+	private String fEntrada;
+	private String carrera;
+	private Boolean estado;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getfEntrada() {
+		return fEntrada;
+	}
+	public void setfEntrada(String fEntrada) {
+		this.fEntrada = fEntrada;
+	}
+	public String getCarrera() {
+		return carrera;
+	}
+	public void setCarrera(String carrera) {
+		this.carrera = carrera;
+	}
+	public Boolean getEstado() {
+		return estado;
+	}
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+	
+	
+
+}
